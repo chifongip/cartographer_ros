6d1b2f9178d6f9257a5e3be1ca24e183fe2607a1
chifongip <chifongip@github.com>
2023-12-12 10:34:48 +0800

    update localization on 20231212

--- cartographer_ros/configuration_files/maxbot_2d.lua
+++ cartographer_ros/configuration_files/maxbot_2d.lua
@@ -56,12 +56,12 @@ TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10. 
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 10. -- 1
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5. -- 10
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 20. -- 40
+-- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 10. -- 1
+-- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5. -- 10
+-- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 20. -- 40
 
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
-POSE_GRAPH.optimize_every_n_nodes = 70 -- 35
+POSE_GRAPH.optimize_every_n_nodes = 35 -- 35
 -- POSE_GRAPH.global_sampling_ratio = 0.002 -- 0.003
 -- POSE_GRAPH.constraint_builder.sampling_ratio = 0.25 -- 0.3
 -- POSE_GRAPH.constraint_builder.max_constraint_distance = 25 
