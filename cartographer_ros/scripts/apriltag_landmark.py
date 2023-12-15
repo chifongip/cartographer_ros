22bcdad83554141a5234edcd4392231225d3b7f3
chifongip <chifongip@github.com>
2023-12-15 17:57:10 +0800

    update landmark params on 20231215

--- cartographer_ros/scripts/apriltag_landmark.py
+++ cartographer_ros/scripts/apriltag_landmark.py
@@ -10,10 +10,12 @@ from geometry_msgs.msg import PoseStamped, Pose
 
 def landmarkCallback(data):
     landmark_pub = rospy.Publisher('landmark', LandmarkList, queue_size=10)
-
+    tf_ros = tf.TransformerROS()
 
     landmark_data = LandmarkList()
-    landmark_data.header = data.header
+    landmark_data.header.seq = data.header.seq
+    landmark_data.header.stamp = data.header.stamp
+    landmark_data.header.frame_id = "imu_link"
 
     if not data.detections:
         landmark_pub.publish(landmark_data)
@@ -21,11 +23,30 @@ def landmarkCallback(data):
         for i in range(len(data.detections)):
             landmark_entry = LandmarkEntry()
             landmark_entry.id = str(data.detections[i].id)
-            landmark_entry.tracking_from_landmark_transform.position = data.detections[i].pose.pose.pose.position
-            landmark_entry.tracking_from_landmark_transform.orientation = data.detections[i].pose.pose.pose.orientation
-            landmark_entry.translation_weight = 1.0
-            landmark_entry.rotation_weight = 1.0    
+
+            usb_cam_link_tag_t = [data.detections[i].pose.pose.pose.position.x, data.detections[i].pose.pose.pose.position.y, data.detections[i].pose.pose.pose.position.z]
+            usb_cam_link_tag_R = [data.detections[i].pose.pose.pose.orientation.x, data.detections[i].pose.pose.pose.orientation.y, data.detections[i].pose.pose.pose.orientation.z, data.detections[i].pose.pose.pose.orientation.w]
+            usb_cam_link_tag_g = tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_R)
+            imu_link_usb_cam_link_g = tf_ros.fromTranslationRotation([0.158, 0.000, 0.028], [-0.500, 0.500, -0.500, 0.500])
+            
+            imu_link_tag_g = np.matmul(imu_link_usb_cam_link_g, usb_cam_link_tag_g)
+
+            imu_link_tag_t = imu_link_tag_g[:3, 3]
+            # imu_link_tag_R = imu_link_tag_g[:3, :3]
+            imu_link_tag_q = tf.transformations.quaternion_from_matrix(imu_link_tag_g)
+
+            landmark_entry.tracking_from_landmark_transform.position.x = imu_link_tag_t[0]
+            landmark_entry.tracking_from_landmark_transform.position.y = imu_link_tag_t[1]
+            landmark_entry.tracking_from_landmark_transform.position.z = imu_link_tag_t[2]
+            landmark_entry.tracking_from_landmark_transform.orientation.x = imu_link_tag_q[0]
+            landmark_entry.tracking_from_landmark_transform.orientation.y = imu_link_tag_q[1]
+            landmark_entry.tracking_from_landmark_transform.orientation.z = imu_link_tag_q[2]
+            landmark_entry.tracking_from_landmark_transform.orientation.w = imu_link_tag_q[3]
+
+            landmark_entry.translation_weight = 10.0
+            landmark_entry.rotation_weight = 5.0    
             landmark_data.landmarks.append(landmark_entry)
+
         landmark_pub.publish(landmark_data)
 
 
