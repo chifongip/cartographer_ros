25b9a849373b70b744e3445b3c968a2cc545392b
chifongip <chifongip@github.com>
2023-12-21 16:18:15 +0800

    update reset pose condition on 20231221

--- cartographer_ros/scripts/update_robot_pose.py
+++ cartographer_ros/scripts/update_robot_pose.py
@@ -30,43 +30,45 @@ def poseCallback(data):
         # tag w.r.t. map
         tag_detected = []
         for i in range(len(data.detections)):
-            tag_detected.append(data.detections[i])
+            if data.detections[i].pose.pose.pose.position.z <= 2.0:
+                tag_detected.append(data.detections[i])
         
-        tag_sorted = sorted(tag_detected, key=lambda tag_detected: tag_detected.pose.pose.pose.position.z)
+        if tag_detected:
+            tag_sorted = sorted(tag_detected, key=lambda tag_detected: tag_detected.pose.pose.pose.position.z)
 
-        for tag in tag_locations:
-            if tag["id"] == int(''.join(map(str, tag_sorted[0].id))): 
-                break
-        map_tag_g = tf_ros.fromTranslationRotation([tag["x"], tag["y"], tag["z"]], [tag["qx"], tag["qy"], tag["qz"], tag["qw"]]) 
-        
-        # tag w.r.t. usb_cam_link
-        usb_cam_link_tag_t = [tag_sorted[0].pose.pose.pose.position.x, tag_sorted[0].pose.pose.pose.position.y, tag_sorted[0].pose.pose.pose.position.z]
-        usb_cam_link_tag_R = [tag_sorted[0].pose.pose.pose.orientation.x, tag_sorted[0].pose.pose.pose.orientation.y, tag_sorted[0].pose.pose.pose.orientation.z, tag_sorted[0].pose.pose.pose.orientation.w]
-        usb_cam_link_tag_g = tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_R)
+            for tag in tag_locations:
+                if tag["id"] == int(''.join(map(str, tag_sorted[0].id))): 
+                    break
+            map_tag_g = tf_ros.fromTranslationRotation([tag["x"], tag["y"], tag["z"]], [tag["qx"], tag["qy"], tag["qz"], tag["qw"]]) 
+            
+            # tag w.r.t. usb_cam_link
+            usb_cam_link_tag_t = [tag_sorted[0].pose.pose.pose.position.x, tag_sorted[0].pose.pose.pose.position.y, tag_sorted[0].pose.pose.pose.position.z]
+            usb_cam_link_tag_R = [tag_sorted[0].pose.pose.pose.orientation.x, tag_sorted[0].pose.pose.pose.orientation.y, tag_sorted[0].pose.pose.pose.orientation.z, tag_sorted[0].pose.pose.pose.orientation.w]
+            usb_cam_link_tag_g = tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_R)
 
-        # usb_cam w.r.t. base_link
-        base_link_usb_cam_link_g = tf_ros.fromTranslationRotation([0.220, 0.000, 0.118], [-0.500, 0.500, -0.500, 0.500])
+            # usb_cam w.r.t. base_link
+            base_link_usb_cam_link_g = tf_ros.fromTranslationRotation([0.220, 0.000, 0.118], [-0.500, 0.500, -0.500, 0.500])
 
-        # calculation of base_link w.r.t. map
-        tag_usb_cam_link_g = np.linalg.inv(usb_cam_link_tag_g)                  # usb_cam_link w.r.t. tag
-        usb_cam_link_base_link_g = np.linalg.inv(base_link_usb_cam_link_g)      # base_link w.r.t. usb_cam_link
+            # calculation of base_link w.r.t. map
+            tag_usb_cam_link_g = np.linalg.inv(usb_cam_link_tag_g)                  # usb_cam_link w.r.t. tag
+            usb_cam_link_base_link_g = np.linalg.inv(base_link_usb_cam_link_g)      # base_link w.r.t. usb_cam_link
 
-        map_base_link_g = np.matmul(map_tag_g, np.matmul(tag_usb_cam_link_g, usb_cam_link_base_link_g))
+            map_base_link_g = np.matmul(map_tag_g, np.matmul(tag_usb_cam_link_g, usb_cam_link_base_link_g))
 
-        # get rotation and translation matrix from transformation matrix 
-        map_base_link_t = map_base_link_g[:3, 3]
-        # map_base_link_R = map_base_link_g[:3, :3]
-        map_base_link_q = tf.transformations.quaternion_from_matrix(map_base_link_g)
+            # get rotation and translation matrix from transformation matrix 
+            map_base_link_t = map_base_link_g[:3, 3]
+            # map_base_link_R = map_base_link_g[:3, :3]
+            map_base_link_q = tf.transformations.quaternion_from_matrix(map_base_link_g)
 
-        map_base_link_data.pose.pose.position.x = map_base_link_t[0]
-        map_base_link_data.pose.pose.position.y = map_base_link_t[1]
-        map_base_link_data.pose.pose.position.z = map_base_link_t[2]
-        map_base_link_data.pose.pose.orientation.x = map_base_link_q[0]
-        map_base_link_data.pose.pose.orientation.y = map_base_link_q[1]
-        map_base_link_data.pose.pose.orientation.z = map_base_link_q[2]
-        map_base_link_data.pose.pose.orientation.w = map_base_link_q[3]
+            map_base_link_data.pose.pose.position.x = map_base_link_t[0]
+            map_base_link_data.pose.pose.position.y = map_base_link_t[1]
+            map_base_link_data.pose.pose.position.z = map_base_link_t[2]
+            map_base_link_data.pose.pose.orientation.x = map_base_link_q[0]
+            map_base_link_data.pose.pose.orientation.y = map_base_link_q[1]
+            map_base_link_data.pose.pose.orientation.z = map_base_link_q[2]
+            map_base_link_data.pose.pose.orientation.w = map_base_link_q[3]
 
-        map_base_link_pub.publish(map_base_link_data)
+            map_base_link_pub.publish(map_base_link_data)
 
 
 def posePublisher():
