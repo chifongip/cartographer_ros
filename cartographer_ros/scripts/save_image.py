cd55eba10ba72e2624762a4c75317b343b58816a
chifongip <chifongip@github.com>
2023-12-27 09:07:23 +0800

    update save image node on 20231227

--- cartographer_ros/scripts/save_image.py
+++ cartographer_ros/scripts/save_image.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python
-
 import rospy
 from sensor_msgs.msg import Image
 import cv2
 import os
+import time 
+
 
 def image_callback(msg):
+    global cv_image
     try:
         cv_image = bridge.imgmsg_to_cv2(msg)
     except CvBridgeError as e:
         print(e)
 
-    # Save the image
-    filename = 'img/image{}.jpg'.format(image_callback.count)
-    cv2.imwrite(filename, cv_image)
-    image_callback.count += 1
 
-if __name__ == '__main__':
+def imageRecorder():
     rospy.init_node('image_listener')
     bridge = CvBridge()
     image_topic = "/usb_cam"
     rospy.Subscriber(image_topic, Image, image_callback)
     if not os.path.exists('img'):
         os.makedirs('img')
-    image_callback.count = 1
-    rate = rospy.Rate(1) # 1 Hz
-    while not rospy.is_shutdown():
-        rospy.spin_once()
-        rate.sleep()
+    count = 1
+
+    while(1):
+        filename = 'img/image{}.jpg'.format(count)
+        cv2.imwrite(filename, cv_image)
+        count += 1
+        time.sleep(3)
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        imageRecorder()
+    except rospy.ROSInterruptException:
+        pass
