d0c42dd8608f31b8e904765fdb9b55fa9184136f
chifongip <chifongip@github.com>
2023-12-28 10:36:39 +0800

    update xy and yaw tolerance in reset pose node on 20231228

--- cartographer_ros/scripts/update_robot_pose.py
+++ cartographer_ros/scripts/update_robot_pose.py
@@ -8,8 +8,6 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, Pose
 from nav_msgs.msg import Odometry
 
 
-tf_ros = tf.TransformerROS()
-
 def odomCallback(data):
     global curr_linear_vel_x, curr_angular_vel_z
     curr_linear_vel_x = data.twist.twist.linear.x
@@ -17,7 +15,8 @@ def odomCallback(data):
 
 
 def poseCallback(data): 
-    if data.detections:
+    # linear and angular velocity threshold for updating the robot's pose
+    if data.detections and curr_linear_vel_x <= max_linear_vel_x and curr_angular_vel_z <= max_angular_vel_z:
         map_base_link_pub = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=10)
 
         map_base_link_data = PoseWithCovarianceStamped()
@@ -31,23 +30,30 @@ def poseCallback(data):
             # distance between tag and base_link
             if data.detections[i].pose.pose.pose.position.z <= max_detection_dist: 
                 tag_detected.append(data.detections[i])
-        
-        # linear and angular velocity threshold for updating the robot's pose  
-        if tag_detected and curr_linear_vel_x <= max_linear_vel_x and curr_angular_vel_z <= max_angular_vel_z:
+    
+        tf_listener.waitForTransform("map", "base_link", rospy.Time(0), rospy.Duration(1.0))
+        (map_base_link_actual_t, map_base_link_actual_q) = tf_listener.lookupTransform("map", "base_link", rospy.Time(0))
+        xy_actual = np.array([map_base_link_actual_t[0], map_base_link_actual_t[1]])                      # actual xy position
+        (_, _, yaw_actual) = tf.transformations.euler_from_quaternion(map_base_link_actual_q)   # actual yaw angle
+  
+        if tag_detected:
             tag_sorted = sorted(tag_detected, key=lambda tag_detected: tag_detected.pose.pose.pose.position.z)
 
             for tag in tag_locations:
                 if tag["id"] == int(''.join(map(str, tag_sorted[0].id))): 
                     break
-            map_tag_g = tf_ros.fromTranslationRotation([tag["x"], tag["y"], tag["z"]], [tag["qx"], tag["qy"], tag["qz"], tag["qw"]]) 
             
+            map_tag_t = [tag["x"], tag["y"], tag["z"]]
+            map_tag_q = [tag["qx"], tag["qy"], tag["qz"], tag["qw"]]
+            map_tag_g = tf_ros.fromTranslationRotation(map_tag_t, map_tag_q) 
+
             # tag w.r.t. usb_cam_link
             usb_cam_link_tag_t = [tag_sorted[0].pose.pose.pose.position.x, tag_sorted[0].pose.pose.pose.position.y, tag_sorted[0].pose.pose.pose.position.z]
-            usb_cam_link_tag_R = [tag_sorted[0].pose.pose.pose.orientation.x, tag_sorted[0].pose.pose.pose.orientation.y, tag_sorted[0].pose.pose.pose.orientation.z, tag_sorted[0].pose.pose.pose.orientation.w]
-            usb_cam_link_tag_g = tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_R)
+            usb_cam_link_tag_q = [tag_sorted[0].pose.pose.pose.orientation.x, tag_sorted[0].pose.pose.pose.orientation.y, tag_sorted[0].pose.pose.pose.orientation.z, tag_sorted[0].pose.pose.pose.orientation.w]
+            usb_cam_link_tag_g = tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_q)
 
             # usb_cam w.r.t. base_link
-            base_link_usb_cam_link_g = tf_ros.fromTranslationRotation(base_link_usb_cam_link_t, base_link_usb_cam_link_R)
+            base_link_usb_cam_link_g = tf_ros.fromTranslationRotation(base_link_usb_cam_link_t, base_link_usb_cam_link_q)
 
             # calculation of base_link w.r.t. map
             tag_usb_cam_link_g = np.linalg.inv(usb_cam_link_tag_g)                  # usb_cam_link w.r.t. tag
@@ -60,35 +66,46 @@ def poseCallback(data):
             # map_base_link_R = map_base_link_g[:3, :3]
             map_base_link_q = tf.transformations.quaternion_from_matrix(map_base_link_g)
 
-            map_base_link_data.pose.pose.position.x = map_base_link_t[0]
-            map_base_link_data.pose.pose.position.y = map_base_link_t[1]
-            map_base_link_data.pose.pose.position.z = map_base_link_t[2]
-            map_base_link_data.pose.pose.orientation.x = map_base_link_q[0]
-            map_base_link_data.pose.pose.orientation.y = map_base_link_q[1]
-            map_base_link_data.pose.pose.orientation.z = map_base_link_q[2]
-            map_base_link_data.pose.pose.orientation.w = map_base_link_q[3]
+            xy_detect = np.array([map_base_link_t[0], map_base_link_t[1]])
+            (_, _, yaw_detect) = tf.transformations.euler_from_quaternion(map_base_link_q)
 
-            map_base_link_pub.publish(map_base_link_data)
+            xy_diff = np.linalg.norm(xy_actual - xy_detect)
+            yaw_diff = abs(yaw_actual - yaw_detect)
+
+            if xy_diff > xy_tolerance or yaw_diff > yaw_tolerance:
+                map_base_link_data.pose.pose.position.x = map_base_link_t[0]
+                map_base_link_data.pose.pose.position.y = map_base_link_t[1]
+                map_base_link_data.pose.pose.position.z = map_base_link_t[2]
+                map_base_link_data.pose.pose.orientation.x = map_base_link_q[0]
+                map_base_link_data.pose.pose.orientation.y = map_base_link_q[1]
+                map_base_link_data.pose.pose.orientation.z = map_base_link_q[2]
+                map_base_link_data.pose.pose.orientation.w = map_base_link_q[3]
+
+                map_base_link_pub.publish(map_base_link_data)
 
 
 def posePublisher():
-    global tag_locations, max_detection_dist, base_link_usb_cam_link_t, base_link_usb_cam_link_R, max_linear_vel_x, max_angular_vel_z
+    global tag_locations, max_detection_dist, base_link_usb_cam_link_t, base_link_usb_cam_link_q, max_linear_vel_x, max_angular_vel_z, \
+    xy_tolerance, yaw_tolerance, tf_ros, tf_listener
     
     tag_locations = rospy.get_param("update_robot_pose/tag_locations")    # get tag locations from yaml 
     max_detection_dist = rospy.get_param("update_robot_pose/max_detection_dist")
     max_linear_vel_x = rospy.get_param("update_robot_pose/max_linear_vel_x")
     max_angular_vel_z = rospy.get_param("update_robot_pose/max_angular_vel_z")
+    xy_tolerance = rospy.get_param("update_robot_pose/xy_tolerance")
+    yaw_tolerance = rospy.get_param("update_robot_pose/yaw_tolerance")
 
     rospy.init_node('posePublisher', anonymous=True)
 
+    rospy.Subscriber("tag_detections", AprilTagDetectionArray, poseCallback)
+    rospy.Subscriber("odom", Odometry, odomCallback)
+
+    tf_ros = tf.TransformerROS()
     tf_listener = tf.TransformListener()
 
     # get transformation between base_link and usb_cam_link from tf_tree
     tf_listener.waitForTransform("base_link", "usb_cam_link", rospy.Time(0), rospy.Duration(1.0))
-    (base_link_usb_cam_link_t, base_link_usb_cam_link_R) = tf_listener.lookupTransform("base_link", "usb_cam_link", rospy.Time(0))
-
-    rospy.Subscriber("tag_detections", AprilTagDetectionArray, poseCallback)
-    rospy.Subscriber("odom", Odometry, odomCallback)
+    (base_link_usb_cam_link_t, base_link_usb_cam_link_q) = tf_listener.lookupTransform("base_link", "usb_cam_link", rospy.Time(0))
 
     rospy.loginfo("Getting transformation from base_link to map.")
     
