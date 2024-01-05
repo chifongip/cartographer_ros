fcdc5062dcf508c91412123dac3a9e5be2d49ed4
chifongip <chifongip@github.com>
2024-01-05 15:52:30 +0800

    update imu_convert node on 20240105

--- cartographer_ros/scripts/imu_convert.py
+++ cartographer_ros/scripts/imu_convert.py
@@ -2,29 +2,25 @@
 import rospy
 from sensor_msgs.msg import Imu
 
-
-def imuCallback(data):
-    imu_pub = rospy.Publisher('imu', Imu, queue_size=10)
-
-    imu_data = data
-    imu_data.angular_velocity.x = imu_data.angular_velocity.x * 0.0174532925
-    imu_data.angular_velocity.y = imu_data.angular_velocity.y * 0.0174532925
-    imu_data.angular_velocity.z = imu_data.angular_velocity.z * 0.0174532925
-    imu_pub.publish(imu_data)
+class imuConvert: 
+    def __init__(self):
+        self.imu_sub = rospy.Subscriber("imu/data", Imu, self.imuCallback, queue_size=1)
+        self.imu_pub = rospy.Publisher('imu', Imu, queue_size=1)
 
 
-def imuConvert():
-    rospy.init_node('imuConvert', anonymous=True)
+    def imuCallback(self, data):
+        imu_data = data
+        imu_data.angular_velocity.x = imu_data.angular_velocity.x * 0.0174532925
+        imu_data.angular_velocity.y = imu_data.angular_velocity.y * 0.0174532925
+        imu_data.angular_velocity.z = imu_data.angular_velocity.z * 0.0174532925
+        self.imu_pub.publish(imu_data)
 
-    rospy.Subscriber("imu/data", Imu, imuCallback)
-
-    rospy.loginfo("Converting IMU angular velocity from deg/s to rad/s.")
-    
-    rospy.spin()
-    
 
 if __name__ == '__main__':
     try:
-        imuConvert()
+        rospy.init_node('imuConvert', anonymous=True)
+        imuConvert = imuConvert()
+        rospy.loginfo("Converting IMU angular velocity from deg/s to rad/s.")
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
