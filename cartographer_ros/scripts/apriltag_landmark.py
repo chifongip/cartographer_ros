eff2b27c6cd3f5119937993fe67d30f39e63c760
chifongip <chifongip@github.com>
2024-09-03 17:05:18 +0800

    update tracking frame as base_link

--- cartographer_ros/scripts/apriltag_landmark.py
+++ cartographer_ros/scripts/apriltag_landmark.py
@@ -16,9 +16,48 @@ class landmarkPublisher:
 
 
     def landmarkCallback(self, data):
+        # # tracking_frame: imu_link
+        # landmark_data = LandmarkList()
+        # landmark_data.header.stamp = data.header.stamp
+        # landmark_data.header.frame_id = "imu_link"
+
+        # if not data.detections:
+        #     self.landmark_pub.publish(landmark_data)
+        # else:
+        #     for i in range(len(data.detections)):
+        #         landmark_entry = LandmarkEntry()
+        #         landmark_entry.id = str(data.detections[i].id)
+
+        #         usb_cam_link_tag_t = [data.detections[i].pose.pose.pose.position.x, data.detections[i].pose.pose.pose.position.y, data.detections[i].pose.pose.pose.position.z]
+        #         usb_cam_link_tag_R = [data.detections[i].pose.pose.pose.orientation.x, data.detections[i].pose.pose.pose.orientation.y, data.detections[i].pose.pose.pose.orientation.z, data.detections[i].pose.pose.pose.orientation.w]
+        #         usb_cam_link_tag_g = self.tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_R)
+        #         imu_link_usb_cam_link_g = self.tf_ros.fromTranslationRotation([0.158, 0.000, 0.028], [-0.500, 0.500, -0.500, 0.500])
+                
+        #         imu_link_tag_g = np.matmul(imu_link_usb_cam_link_g, usb_cam_link_tag_g)
+
+        #         imu_link_tag_t = imu_link_tag_g[:3, 3]
+        #         # imu_link_tag_R = imu_link_tag_g[:3, :3]
+        #         imu_link_tag_q = tf.transformations.quaternion_from_matrix(imu_link_tag_g)
+
+        #         landmark_entry.tracking_from_landmark_transform.position.x = imu_link_tag_t[0]
+        #         landmark_entry.tracking_from_landmark_transform.position.y = imu_link_tag_t[1]
+        #         landmark_entry.tracking_from_landmark_transform.position.z = imu_link_tag_t[2]
+        #         landmark_entry.tracking_from_landmark_transform.orientation.x = imu_link_tag_q[0]
+        #         landmark_entry.tracking_from_landmark_transform.orientation.y = imu_link_tag_q[1]
+        #         landmark_entry.tracking_from_landmark_transform.orientation.z = imu_link_tag_q[2]
+        #         landmark_entry.tracking_from_landmark_transform.orientation.w = imu_link_tag_q[3]
+
+        #         landmark_entry.translation_weight = 10.0
+        #         landmark_entry.rotation_weight = 10.0    
+        #         landmark_data.landmarks.append(landmark_entry)
+
+        #     self.landmark_pub.publish(landmark_data)
+
+
+        # tracking_frame: base_link
         landmark_data = LandmarkList()
         landmark_data.header.stamp = data.header.stamp
-        landmark_data.header.frame_id = "imu_link"
+        landmark_data.header.frame_id = "base_link"
 
         if not data.detections:
             self.landmark_pub.publish(landmark_data)
@@ -30,64 +69,27 @@ class landmarkPublisher:
                 usb_cam_link_tag_t = [data.detections[i].pose.pose.pose.position.x, data.detections[i].pose.pose.pose.position.y, data.detections[i].pose.pose.pose.position.z]
                 usb_cam_link_tag_R = [data.detections[i].pose.pose.pose.orientation.x, data.detections[i].pose.pose.pose.orientation.y, data.detections[i].pose.pose.pose.orientation.z, data.detections[i].pose.pose.pose.orientation.w]
                 usb_cam_link_tag_g = self.tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_R)
-                imu_link_usb_cam_link_g = self.tf_ros.fromTranslationRotation([0.158, 0.000, 0.028], [-0.500, 0.500, -0.500, 0.500])
+                base_link_usb_cam_link_g = self.tf_ros.fromTranslationRotation([0.220, 0.000, 0.118], [-0.500, 0.500, -0.500, 0.500])
                 
-                imu_link_tag_g = np.matmul(imu_link_usb_cam_link_g, usb_cam_link_tag_g)
+                base_link_tag_g = np.matmul(base_link_usb_cam_link_g, usb_cam_link_tag_g)
 
-                imu_link_tag_t = imu_link_tag_g[:3, 3]
-                # imu_link_tag_R = imu_link_tag_g[:3, :3]
-                imu_link_tag_q = tf.transformations.quaternion_from_matrix(imu_link_tag_g)
+                base_link_tag_t = base_link_tag_g[:3, 3]
+                # base_link_tag_R = base_link_tag_g[:3, :3]
+                base_link_tag_q = tf.transformations.quaternion_from_matrix(base_link_tag_g)
 
-                landmark_entry.tracking_from_landmark_transform.position.x = imu_link_tag_t[0]
-                landmark_entry.tracking_from_landmark_transform.position.y = imu_link_tag_t[1]
-                landmark_entry.tracking_from_landmark_transform.position.z = imu_link_tag_t[2]
-                landmark_entry.tracking_from_landmark_transform.orientation.x = imu_link_tag_q[0]
-                landmark_entry.tracking_from_landmark_transform.orientation.y = imu_link_tag_q[1]
-                landmark_entry.tracking_from_landmark_transform.orientation.z = imu_link_tag_q[2]
-                landmark_entry.tracking_from_landmark_transform.orientation.w = imu_link_tag_q[3]
+                landmark_entry.tracking_from_landmark_transform.position.x = base_link_tag_t[0]
+                landmark_entry.tracking_from_landmark_transform.position.y = base_link_tag_t[1]
+                landmark_entry.tracking_from_landmark_transform.position.z = base_link_tag_t[2]
+                landmark_entry.tracking_from_landmark_transform.orientation.x = base_link_tag_q[0]
+                landmark_entry.tracking_from_landmark_transform.orientation.y = base_link_tag_q[1]
+                landmark_entry.tracking_from_landmark_transform.orientation.z = base_link_tag_q[2]
+                landmark_entry.tracking_from_landmark_transform.orientation.w = base_link_tag_q[3]
 
                 landmark_entry.translation_weight = 10.0
                 landmark_entry.rotation_weight = 10.0    
                 landmark_data.landmarks.append(landmark_entry)
 
             self.landmark_pub.publish(landmark_data)
-
-
-        '''
-            convert tag w.r.t. base_link
-        '''
-        # base_link_tag_pub = rospy.Publisher('base_link_tag', PoseStamped, queue_size=10)
-
-        # base_link_tag_data = PoseStamped()
-        # base_link_tag_data.header.seq = data.header.seq
-        # base_link_tag_data.header.stamp = data.header.stamp
-        # base_link_tag_data.header.frame_id = "base_link"
-
-        # if not data.detections:
-        #     base_link_tag_pub.publish(base_link_tag_data)
-        # else:
-        #     print(len(data.detections))
-
-        #     usb_cam_link_tag_t = [data.detections[0].pose.pose.pose.position.x, data.detections[0].pose.pose.pose.position.y, data.detections[0].pose.pose.pose.position.z]
-        #     usb_cam_link_tag_R = [data.detections[0].pose.pose.pose.orientation.x, data.detections[0].pose.pose.pose.orientation.y, data.detections[0].pose.pose.pose.orientation.z, data.detections[0].pose.pose.pose.orientation.w]
-        #     usb_cam_link_tag_g = self.tf_ros.fromTranslationRotation(usb_cam_link_tag_t, usb_cam_link_tag_R)
-        #     base_link_usb_cam_link_g = self.tf_ros.fromTranslationRotation([0.220, 0.000, 0.118], [-0.500, 0.500, -0.500, 0.500])
-            
-        #     base_link_tag_g = np.matmul(base_link_usb_cam_link_g, usb_cam_link_tag_g)
-
-        #     base_link_tag_t = base_link_tag_g[:3, 3]
-        #     # base_link_tag_R = base_link_tag_g[:3, :3]
-        #     base_link_tag_q = tf.transformations.quaternion_from_matrix(base_link_tag_g)
-
-        #     base_link_tag_data.pose.position.x = base_link_tag_t[0]
-        #     base_link_tag_data.pose.position.y = base_link_tag_t[1]
-        #     base_link_tag_data.pose.position.z = base_link_tag_t[2]
-        #     base_link_tag_data.pose.orientation.x = base_link_tag_q[0]
-        #     base_link_tag_data.pose.orientation.y = base_link_tag_q[1]
-        #     base_link_tag_data.pose.orientation.z = base_link_tag_q[2]
-        #     base_link_tag_data.pose.orientation.w = base_link_tag_q[3]
-
-        #     base_link_tag_pub.publish(base_link_tag_data)
     
 
 if __name__ == '__main__':
